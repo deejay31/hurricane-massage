349edf935026c3e1872a118ff2175f32b3fa1ac6
deejay-salva <deejay.salva@awsys-i.com>
2024-05-14 17:28:55 +0800

    fix(npm):

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,12 +1,1 @@
-self.__RSC_CSS_MANIFEST={
-  "cssImports": {
-    "C:\\nodejs-apps\\hurricane-massage\\src\\app\\layout.tsx": [
-      "C:\\nodejs-apps\\hurricane-massage\\src\\app\\globals.css"
-    ]
-  },
-  "cssModules": {
-    "C:\\nodejs-apps\\hurricane-massage\\src\\app\\page": [
-      "C:\\nodejs-apps\\hurricane-massage\\src\\app\\globals.css"
-    ]
-  }
-}
+self.__RSC_CSS_MANIFEST={"cssImports":{"C:\\nodejs-apps\\hurricane-massage\\src\\app\\layout.tsx":["C:\\nodejs-apps\\hurricane-massage\\src\\app\\globals.css"]},"cssModules":{"C:\\nodejs-apps\\hurricane-massage\\src\\app\\page":["C:\\nodejs-apps\\hurricane-massage\\src\\app\\globals.css"]}}
